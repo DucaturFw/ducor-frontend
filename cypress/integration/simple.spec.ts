5730f2c3f6a290077397d12c91a500a4ffc0fa43
zlumer <zlumer@gmail.com>
2018-08-02 17:30:54 +0300

    basic ci tests

---
+++ cypress/integration/simple.spec.ts
@@ -0,0 +1,29 @@
+describe('My First Test', () =>
+{
+	let URL = 'http://localhost:3090'
+	it('visit main page', () =>
+	{
+		cy.visit(URL)
+	})
+	it('should work with deep links', () =>
+	{
+		cy.visit(`${URL}/construct`)
+
+		cy.contains('ETH/BTC')
+		cy.contains('Generate')
+	})
+	it('generate contract', () =>
+	{
+		cy.visit(URL)
+
+		cy.contains('Cryptocurrency').click()
+		cy.url().should('include', '/construct')
+		cy.contains('ETH/BTC').click()
+		cy.contains('Binance').click()
+		cy.contains('Generate').click()
+
+		cy.url().should('include', "/contractCode")
+		cy.contains("eosiolib/eosio.hpp").contains("EOSLIB_SERIALIZE").contains("YOUR_CONTRACT_NAME")
+		cy.contains("eos_contract_instructions")
+	})
+})
